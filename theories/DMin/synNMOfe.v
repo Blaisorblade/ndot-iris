4aab9065cf22283ca9fd743356315d95b4114156
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2019-03-17 04:01:40 +0100

    Start writing boilerplate to define OFEs

---
+++ theories/DMin/synNMOfe.v
@@ -0,0 +1,115 @@
+From Autosubst Require Export Autosubst.
+From stdpp Require Import base.
+From Coq.ssr Require Import ssreflect.
+From iris.algebra Require Import base.
+
+From DN Require Import autosubst_preds DMin.synNonMutual.
+
+From iris.algebra Require Import ofe.
+
+Section syn_relation.
+  Context `{α : Type}.
+  Notation "'vl'" := (syn α vls).
+  Notation "'tm'" := (syn α tms).
+
+  Context {Rα: relation α}.
+  Context {Rvar: relation var}.
+  Context {Rnat: relation nat}.
+  Inductive sr : ∀ {s}, relation (syn α s) :=
+  | rtv v1 v2 :
+      sr v1 v2 →
+      sr (tv v1) (tv v2)
+  | rtapp ta1 ta2 tb1 tb2:
+      sr ta1 ta2 →
+      sr tb1 tb2 →
+      sr (tapp ta1 tb1) (tapp ta2 tb2)
+  | rtproj v1 v2:
+      sr v1 v2 →
+      sr (tproj v1) (tproj v2)
+  | rtskip t1 t2:
+      sr t1 t2 →
+      sr (tskip t1) (tskip t2)
+  | rvar_vl i1 i2:
+      Rvar i1 i2 →
+      sr (var_vl i1) (var_vl i2)
+  | rvnat n1 n2:
+      Rnat n1 n2 →
+      sr (vnat n1) (vnat n2)
+  | rvabs t1 t2:
+      sr t1 t2 →
+      sr (vabs t1) (vabs t2)
+  | rvpack α1 α2 t1 t2:
+      Rα α1 α2 →
+      sr t1 t2 →
+      sr (vpack α1 t1) (vpack α2 t2)
+  .
+End syn_relation.
+
+Arguments sr {_} _ _ _ {_}.
+
+Require Import Program.Equality.
+
+Section syn_relation_prop.
+  Context `{R1 : relation α, R2 : relation var, R3 : relation nat}.
+
+  Global Instance sr_refl :
+    Reflexive R1 → Reflexive R2 → Reflexive R3 → Reflexive (sr R1 R2 R3 (s := s)).
+  Proof. intros ????; elim; constructor; eauto. Qed.
+  Global Instance sr_sym :
+    Symmetric R1 → Symmetric R2 → Symmetric R3 → Symmetric (sr R1 R2 R3 (s := s)).
+  Proof. induction 4; constructor; eauto. Qed.
+  Global Instance sr_trans :
+    Transitive R1 → Transitive R2 → Transitive R3 → Transitive (sr R1 R2 R3 (s := s)).
+  Proof. induction 4; intro x; dependent induction x; constructor; eauto. Qed.
+  Global Instance sr_equiv :
+    Equivalence R1 → Equivalence R2 → Equivalence R3 → Equivalence (sr R1 R2 R3 (s := s)).
+  Proof. split; apply _. Qed.
+
+  (* A few of these instances; do we really need one per constructor? *)
+  Global Instance var_vl_proper : Proper (R2 ==> sr R1 R2 R3) var_vl.
+  Proof. constructor; auto. Qed.
+  Global Instance vnat_proper : Proper (R3 ==> sr R1 R2 R3) vnat.
+  Proof. constructor; auto. Qed.
+  Global Instance tv_proper : Proper (sr R1 R2 R3 ==> sr R1 R2 R3) tv.
+  Proof. constructor; auto. Qed.
+  Global Instance vpack_proper : Proper (R1 ==> sr R1 R2 R3 ==> sr R1 R2 R3) vpack.
+  Proof. constructor; auto. Qed.
+
+  Global Instance tv_inj : Inj (sr R1 R2 R3) (sr R1 R2 R3) tv.
+  Proof. inversion_clear 1; auto. Qed.
+  Global Instance vpack_inj: Inj2 R1 (sr R1 R2 R3) (sr R1 R2 R3) vpack.
+  Proof. inversion_clear 1; auto. Qed.
+  Global Instance tapp_inj: Inj2 (sr R1 R2 R3) (sr R1 R2 R3) (sr R1 R2 R3) tapp.
+  Proof. inversion_clear 1; auto. Qed.
+  (* Global Instance inr_inj' : Inj R2 (sr R1 R2) inr.
+  Proof. inversion_clear 1; auto. Qed. *)
+End syn_relation_prop.
+
+Global Instance equivSyn : ∀ {α s} `{!Equiv α}, Equiv (syn α s) :=
+  λ α s Eα, sr (≡) (≡) (≡).
+Global Instance distSyn : ∀ {α s} `{!Dist α}, Dist (syn α s) :=
+  λ α s Eα n, sr (dist n) (dist n) (dist n).
+
+Section synOfe.
+  Context {αC: ofeT}.
+  Definition α: Type := αC.
+  Notation "'vl'" := (syn α vls).
+  Notation "'tm'" := (syn α tms).
+
+  Global Instance tv_ne: NonExpansive (@tv α) := _.
+  Global Instance vpack_ne: NonExpansive2 (@vpack α) := _.
+
+  Definition vlOfeMixin s : OfeMixin (syn α s).
+    split.
+    - move => x y; split => Hx.
+      + induction Hx => n; constructor;
+        unfold dist, distSyn in *; eauto.
+        by apply equiv_dist.
+      + induction (Hx 0); constructor; unfold dist, distSyn, equiv, equivSyn in *.
+        * apply IHd => n. by apply (inj _).
+        * apply IHd1 => n.
+        apply (inj _).
+  Abort.
+
+  (* Program Definition synC: ofeT := OfeT vl _. *)
+End synOfe.
